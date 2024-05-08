ca8ca544e4fa1936fa8f2276010026a933972aea
Malen <malen8622@gmail.com>
2024-05-08 20:03:35 +0700

    feat(addToChart): add the whole page

---
+++ assets/js/addChart.js
@@ -0,0 +1,47 @@
+var prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".nav-bar").style.top = "0";
+  } else {
+    document.querySelector(".nav-bar").style.top = "-115px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+const slideShow = function () {
+  document.query;
+};
+
+function openNav() {
+  document.querySelector(".nav-hamburger").classList.add("openNavBar");
+  document.querySelector(".nav-hamburger").classList.remove("closeNavBar");
+  document.querySelector("html").classList.add("overflow-y-hidden")
+}
+function closeNav() {
+  document.querySelector(".nav-hamburger").classList.add("closeNavBar");
+  document.querySelector(".nav-hamburger").classList.remove("openNavBar");
+  document.querySelector("html").classList.remove("overflow-y-hidden")
+}
+
+let currentSlide = 0;
+function pushArrow(n) {
+  const carousel = document.querySelector(" .slideShow ");
+  const images = carousel.querySelectorAll(".slide-list");
+  const imageWidth = images[0].clientWidth;
+
+  currentSlide = Math.max(0, Math.min(currentSlide + n, images.length - 1));
+  carousel.scrollTo({ left: currentSlide * imageWidth, behavior: "smooth" });
+}
+
+let accordionBtn = document.querySelectorAll("#accordion-btn");
+let accordionDropdown = document.querySelectorAll("#accordion-dropdown");
+let plusIcon = document.querySelectorAll("#plus-icon");
+let closeIcon = document.querySelectorAll("#close-icon");
+accordionBtn.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    accordionDropdown[i].classList.toggle("max-h-[300px]");
+    plusIcon[i].classList.toggle("hidden");
+    closeIcon[i].classList.toggle("hidden");
+  });
+});
