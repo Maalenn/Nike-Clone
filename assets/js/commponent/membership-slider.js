6594a4875f9eb89ff3091a461fff716f2b9f9661
Malen <malen8622@gmail.com>
2024-05-28 17:27:03 +0700

    refeactor(homePage): change membership slider to component

--- assets/js/commponent/membership-slider.js
+++ assets/js/commponent/membership-slider.js
@@ -7,26 +7,47 @@ const dataSources = {
 
 const MembershipContainer = (memberContents) =>{
     return `
-        <ul class="w-[100%] grid grid-cols-[repeat(${memberContents.length},450px)] overflow-auto slideShow">
-            ${memberContents.map(items => `
-                <li class="p-[5px] slide-list">
-                    <a class="" href=""> 
-                        <div class="">
-                            <div class="relative">
-                                <div class="">
-                                    <img src="${items.imgMember}" alt="" />
-                                </div>
-                                <div class="absolute bottom-10 left-10 text-white flex flex-col gap-5">
-                                    <h3 class="capitalize text-[18px] ">${items.titleMember}</h3>
-                                    <h1 class="capitalize text-[25px] ">${items.desMember}</h1>
-                                    <button class="capitalize text-[20px] bg-white text-black px-3 py-2 rounded-[30px] max-w-[120px]">${items.btnMember}</button>
-                                </div>
+    <section class="flex justify-center">
+            <section class="max-w-[91%] h-full">
+                <div class="scroll-info container">
+                    <div class="scroll-des">
+                        <h1>Member Benefits</h1>
+                    </div>
+                    <div class="scroll-icon">
+                        <button class="link-arrow" onclick="pushArrow(0,-1)">
+                            <div class="circle-icon icon-left">
+                            <img src="../assets/Image/arrow-left.svg" alt="Left Arrow" />
                             </div>
-                        </div>
-                    </a>
-                </li>
-            `).join("")}
-        </ul>
+                        </button>
+                        <button class="link-arrow" onclick="pushArrow(0,1)">
+                            <div class="circle-icon icon-right">
+                            <img src="../assets/Image/arrow-right.svg" alt="Right Arrow" />
+                            </div>
+                        </button>
+                    </div>
+                </div>
+                <ul class="w-[100%] grid grid-cols-[repeat(${memberContents.length},450px)] overflow-auto slideShow">
+                    ${memberContents.map(items => `
+                        <li class="p-[5px] slide-list">
+                            <a class="" href=""> 
+                                <div class="">
+                                    <div class="relative">
+                                        <div class="">
+                                            <img src="${items.imgMember}" alt="" />
+                                        </div>
+                                        <div class="absolute bottom-10 left-10 text-white flex flex-col gap-5">
+                                            <h3 class="capitalize text-[18px] ">${items.titleMember}</h3>
+                                            <h1 class="capitalize text-[25px] ">${items.desMember}</h1>
+                                            <button class="capitalize text-[20px] bg-white text-black px-3 py-2 rounded-[30px] max-w-[120px]">${items.btnMember}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    `).join("")}
+                </ul>
+            </section>
+        </section>
     `
 }
 
@@ -38,4 +59,5 @@ class MemberShipSlider extends HTMLElement {
   }
 }
 
-customElements.define("membership-slider-component", MemberShipSlider);
+customElements.define("membership-slidercomponent", MemberShipSlider);
+
