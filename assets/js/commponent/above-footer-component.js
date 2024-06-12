12e7d133557a530b178fc5bcbc34a17d1fc119fd
Malen <malen8622@gmail.com>
2024-06-12 11:52:44 +0700

    refactor(homePage): change above footer to dynamic component

--- assets/js/commponent/above-footer-component.js
+++ assets/js/commponent/above-footer-component.js
@@ -1,82 +1,40 @@
-const AboveFooterContainer = () =>{
-    return `
-    <section class="above-footer container-above blur-background">
-        <ul class="above-menu">
-            <h5>Feature</h5>
-            <ul>
-                <li class="above-list">
-                    <a href="">Air Force 1</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Jordan 1</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Metcon</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Air Max 270</a>
-                </li>
-            </ul>
-        </ul>
-        <ul class="above-menu">
-            <h5>Shoes</h5>
-            <ul>
-                <li class="above-list">
-                    <a href="">All Shoes</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Jordan Shoes</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Running Shoes</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Basketball Shoes</a>
-                </li>
-            </ul>
-        </ul>
-        <ul class="above-menu">
-            <h5>Clothing</h5>
-            <ul>
-                <li class="above-list">
-                    <a href="">All Clothing</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Tops & T-shirts</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Shorts</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Hoodies & Pollovers</a>
-                </li>
-            </ul>
-        </ul>
-        <ul class="above-menu">
-            <h5>Kids</h5>
-            <ul>
-                <li class="above-list">
-                    <a href="">Infant & Toddler Shoes</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Kids Shoes</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Kids Basketball Shoes</a>
-                </li>
-                <li class="above-list">
-                    <a href="">Kid Running Shoes</a>
-                </li>
-            </ul>
-        </ul>
-    </section>
-    `
+import { AboveFooterData } from "../data/above-footer-data.js";
+const dataSources = {
+    'above-footer-data': AboveFooterData
 }
+
+const AboveFooterContainer = (aboveFooterContents) => {
+    console.log(aboveFooterContents)
+    return `
+        <section class="above-footer container-above blur-background">
+        ${aboveFooterContents.map((items) =>`
+            <ul class="above-menu">
+                <li>
+                    <h5 >${items.menuTitle}</h5>
+                    <ul class="hidden md:block">
+                        ${items.menuList.map((item) => `
+                            <li class="above-list">
+                                <a href="#">${item}</a>
+                            </li>
+                        `).join('')}
+                    </ul>
+                </li>
+             
+            </ul>
+             `).join('')}
+        </section>
+    `;
+};
+
 
 class AboveFooter extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = AboveFooterContainer();
+        const dataSource = this.getAttribute("data-source");
+        const data = dataSources[dataSource];
+        this.innerHTML = AboveFooterContainer(data);
     }
 }
 
 customElements.define("above-footer-component", AboveFooter)
+
+
