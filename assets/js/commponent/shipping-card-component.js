d8889ee68df706736aeaea5947edd83a6d84a9c0
Malen <malen8622@gmail.com>
2024-06-12 14:43:03 +0700

    reafactor(homePage): change push button to reuseable component

--- assets/js/commponent/shipping-card-component.js
+++ assets/js/commponent/shipping-card-component.js
@@ -6,52 +6,32 @@ const dataSources = {
 
 const ShippingSliderContainer = (shippingContents) => {
   return `
-        <section class="flex justify-center">
-            <section class="max-w-[91%] h-full">
-                <div class="scroll-info container">
-                    <div class="scroll-des">
-                    <h1>Member Benefits</h1>
-                    </div>
-                    <div class="scroll-icon max-[767px]:hidden">
-                        <button href="" class="link-arrow" onclick="pushArrow(1,-1)">
-                            <div class="circle-icon icon-left">
-                            <img src="../assets/Image/arrow-left.svg" alt="" />
-                            </div>
-                        </button>
-                        <button href="" class="link-arrow" onclick="pushArrow(1,1)">
-                            <div class="circle-icon icon-right">
-                            <img src="../assets/Image/arrow-right.svg" alt="" />
-                            </div>
-                        </button>
-                    </div>
-                </div>
-                <ul class="w-[100%] grid  grid-cols-[repeat(${
-                  shippingContents.length
-                },450px)] overflow-auto slideShow container">
-                    ${shippingContents
-                      .map(
-                        (items) => `
-                        <li class="py-[20px] slide-list">
-                            <a class="" href=""> 
+        <ul class="w-[100%] grid  grid-cols-[repeat(${
+            shippingContents.length
+        },450px)] overflow-auto slideShow container">
+            ${shippingContents
+                .map(
+                (items) => `
+                <li class="py-[20px] slide-list">
+                    <a class="" href=""> 
+                        <div class="">
+                            <div class="relative">
                                 <div class="">
-                                    <div class="relative">
-                                        <div class="">
-                                            <img src="${items.imgShip}" alt="" />
-                                        </div>
-                                        <div class="text-center absolute bottom-60 left-20 w-[350px] text-black flex flex-col gap-10">
-                                            <h3 class="text-[28px] font-bold">${items.titleShip}</h3>
-                                            <h1 class="text-[20px] ">${items.desShip}</h1>
-                                        </div>
-                                    </div>
+                                    <img src="${items.imgShip}" alt="" />
                                 </div>
-                            </a>
-                        </li>
-                    `
-                      )
-                      .join("")}
-                </ul>
-            </section>
-        </section>
+                                <div class="text-center absolute bottom-60 left-20 w-[350px] text-black flex flex-col gap-10">
+                                    <h3 class="text-[28px] font-bold">${items.titleShip}</h3>
+                                    <h1 class="text-[20px] ">${items.desShip}</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            `
+                )
+                .join("")}
+        </ul>
+            
     `;
 };
 
